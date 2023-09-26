d3c5e5459920af584c7927dc13792b9ae40f5552
Vishal Ayyappan <120817831+vishal-ayyappan@users.noreply.github.com>
2023-09-26 16:12:19 +0530

    Add files via upload

--- 63.2022/codes/main.py
+++ 63.2022/codes/main.py
@@ -1,33 +1,23 @@
+import scipy.integrate as spi
 import numpy as np
 
-def p_Y_given_XA(y):
-    # Replace with the actual probability density function p(Y|X_A)
-    # For example, if it's a Gaussian distribution with mean mu and std sigma:
-    mu = 2
-    sigma = 1
-    return 1 / (sigma * np.sqrt(2 * np.pi)) * np.exp(-0.5 * ((y - mu) / sigma)**2)
+# Define the conditional probability density functions
+def fY_given_xA(y):
+    return np.exp(-(y+1))
 
-def p_Y_given_XB(y):
-    # Replace with the actual probability density function p(Y|X_B)
-    # For example, if it's a Gaussian distribution with mean mu and std sigma:
-    mu = 5
-    sigma = 1
-    return 1 / (sigma * np.sqrt(2 * np.pi)) * np.exp(-0.5 * ((y - mu) / sigma)**2)
+def fY_given_xB(y):
+    return np.exp(y-1) * (1 - (y < -1))
 
-def conditional_probability(Y, X):
-    if X == 'A':
-        p_Y_given_X = p_Y_given_XA
-    elif X == 'B':
-        p_Y_given_X = p_Y_given_XB
-    else:
-        raise ValueError("X must be 'A' or 'B'")
-    
-    return p_Y_given_X(Y)
+# Define the intervals for integration
+interval_xA = (0, 1)
+interval_xB = (-1, 0)
 
-# Example usage:
-Y_value = 3  # Replace with the actual value of Y
-X_value = 'A'  # Replace with the actual value of X (either 'A' or 'B')
+# Integrate the conditional probability density functions
+PexA, _ = spi.quad(fY_given_xA, *interval_xA)
+PexB, _ = spi.quad(fY_given_xB, *interval_xB)
 
-result = conditional_probability(Y_value, X_value)
-print(result)
+# Calculate the total probability of error
+Pe = PexA*0.5 + PexB*0.5
+
+print(f"The probability of error (Pe) is: {Pe}")
 
